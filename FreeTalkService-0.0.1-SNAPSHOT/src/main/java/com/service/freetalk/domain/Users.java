d93d8a5e9d0607ae98b061cb59c28b43a3efb276
cuckoo03 <cuckoo03@gmail.com>
2013-09-06 13:13:32 +0900

    .

---
+++ FreeTalkService-0.0.1-SNAPSHOT/src/main/java/com/service/freetalk/domain/Users.java
@@ -0,0 +1,49 @@
+package com.service.freetalk.domain;
+
+public class Users {
+	private Integer no;
+	private String id;
+	private String password;
+	private String email;
+	private String deviceId;
+
+	public Integer getNo() {
+		return no;
+	}
+
+	public void setNo(Integer no) {
+		this.no = no;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDeviceId() {
+		return deviceId;
+	}
+
+	public void setDeviceId(String deviceId) {
+		this.deviceId = deviceId;
+	}
+}
