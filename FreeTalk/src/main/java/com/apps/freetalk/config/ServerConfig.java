003ff0ffd29d92eb51c46837d9100b4936982679
cuckoo03 <cuckoo03@gmail.com>
2013-09-25 14:05:09 +0900

    /

--- FreeTalk/src/main/java/com/apps/freetalk/config/ServerConfig.java
+++ FreeTalk/src/main/java/com/apps/freetalk/config/ServerConfig.java
@@ -1,8 +1,8 @@
 package com.apps.freetalk.config;
 
 public class ServerConfig {
-	public static final String SERVER_URL = "http://54.250.217.195:8080/FreeTalkService";
-//	public static final String SERVER_URL = "http://192.168.0.3:8080/FreeTalkService";
+//	public static final String SERVER_URL = "http://54.250.217.195:8080/FreeTalkService";
+	public static final String SERVER_URL = "http://192.168.0.3:8080/FreeTalkService";
 	public static final String PROJECT_ID = "785873295611";
 	private static final int CONN_TIMEOUT = 30;
 	private static final int READ_TIMEOUT = 30;
