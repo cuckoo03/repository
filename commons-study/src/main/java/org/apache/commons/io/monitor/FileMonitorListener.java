9e09176946e857c187186089045afd1b85a7154c
cuckoo03 <cuckoo03@gmail.com>
2013-11-25 14:34:44 +0900

    .

--- commons-study/src/main/java/org/apache/commons/io/monitor/FileMonitorListener.java
+++ commons-study/src/main/java/org/apache/commons/io/monitor/FileMonitorListener.java
@@ -9,22 +9,27 @@ public class FileMonitorListener implements FileAlterationListener {
 		this.monitor = monitor;
 	}
 
+	@Override
 	public void onStart(FileAlterationObserver observer) {
 		System.out.println("start");
 	}
 
+	@Override
 	public void onDirectoryCreate(File directory) {
 		System.out.println("directory create:" + directory);
 	}
 
+	@Override
 	public void onDirectoryChange(File directory) {
 		System.out.println("directory change:" + directory);
 	}
 
+	@Override
 	public void onDirectoryDelete(File directory) {
 		System.out.println("directory delete:" + directory);
 	}
 
+	@Override
 	public void onFileCreate(File file) {
 		System.out.println("file create:" + file);
 		if ("end".equals(file.getName().toLowerCase())) {
@@ -36,14 +41,17 @@ public class FileMonitorListener implements FileAlterationListener {
 		}
 	}
 
+	@Override
 	public void onFileChange(File file) {
 		System.out.println("file change:" + file);
 	}
 
+	@Override
 	public void onFileDelete(File file) {
 		System.out.println("file delete:" + file);
 	}
 
+	@Override
 	public void onStop(FileAlterationObserver observer) {
 		System.out.println("stop");
 	}
