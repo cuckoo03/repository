0f60527bfc053ccd8e6e7c95684670708ffd066b
cuckoo03 <cuckoo03@gmail.com>
2015-04-04 06:25:56 +0900

    .

---
+++ spark-study/src/main/java/com/java/SimpleApp.java
@@ -0,0 +1,20 @@
+package com.java;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class SimpleApp {
+	public static void main(String[] args) {
+		String logFile = "/usr/local/spark/README.md";
+		SparkConf conf = new SparkConf().setAppName("Simple application");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		JavaRDD<String> logData = sc.textFile(logFile).filter(
+				s -> s.contains("a"));
+		long numAs = logData.count();
+
+		long numBs = sc.textFile(logFile).filter(s -> s.contains("b")).count();
+
+		System.out.println("Lines with a:" + numAs + ", " + numBs);
+	}
+}
---
+++ spark-study/src/main/scala/com/scala/SimpleApp.scala
@@ -0,0 +1,15 @@
+package com.scala
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object SimpleApp extends App {
+	val logFile = "/usr/local/spark/README.md"
+	val conf = new SparkConf().setAppName("Simple application")
+	val sc = new SparkContext(conf)
+	val logData = sc.textFile(logFile, 2).cache()
+	val numAs = logData.filter(line => line.contains("a")).count()
+	val numBs = logData.filter(line => line.contains("b")).count()
+
+	println("a:" + numAs + "," + numBs)
+}
