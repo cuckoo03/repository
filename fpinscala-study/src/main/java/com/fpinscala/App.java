48e8ac0e0e6eec003fbb1ec111f21c1fa66b0f5c
cuckoo03 <cuckoo03@gmail.com>
2015-12-11 20:56:35 +0900

    commit

---
+++ fpinscala-study/src/main/java/com/fpinscala/App.java
@@ -0,0 +1,13 @@
+package com.fpinscala;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
