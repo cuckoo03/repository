517f75a5ad68941232fa5ada350b526f79aa3067
cuckoo03@gmail.com <wolfen11>
2017-09-22 09:16:09 +0900

    #

---
+++ ml-study/src/main/java/com/mlstudy/App.java
@@ -0,0 +1,13 @@
+package com.mlstudy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
