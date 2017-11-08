d9c0b51c2fcfb3c60c8a8f47a48205f0866a552f
cuckoo03@gmail.com <wolfen11>
2017-11-08 09:56:11 +0900

    #

--- groovy-study/src/main/groovy/com/groovy/release/r2_3/Java8StreamExam.groovy
+++ groovy-study/src/main/groovy/com/groovy/release/r2_3/Java8StreamExam.groovy
@@ -4,12 +4,25 @@ import groovy.transform.TypeChecked
 
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.util.function.Function
+import java.util.function.Predicate;
 import java.util.stream.IntStream
 
 @TypeChecked
 class Java8StreamExam {
 	static main(args) {
+		def upperString = { String it -> it.toUpperCase() } as Function
+		
 		IntStream.range(1, 100).forEach { println it }
-		Files.lines(Paths.get("")).map{ it.toUpperCase()}.forEach{ println it }
+//		Files.lines(Paths.get("")).map{ it.toUpperCase() }.forEach{ println it }
+		def intResult = (1..10).stream()
+				.filter( { int it -> it % 2 == 0 } as Predicate)
+				.map( { int it -> it + "=${it}" } as Function).collect()
+		println intResult
+		
+		def stringResult = ["a", "b", "c"].stream().filter { it == "b" }
+		.map (upperString).collect()
+		println stringResult
 	}
+	
 }
