a90b434b9c72416cbe0059a0960fbd198bb0dccd
cuckoo03@gmail.com <wolfen11>
2018-07-16 22:56:20 +0900

    #

--- elasticsearch-study/src/main/groovy/com/elasticsearch/client/DocumentInserter2.groovy
+++ elasticsearch-study/src/main/groovy/com/elasticsearch/client/DocumentInserter2.groovy
@@ -1,63 +1,82 @@
 package com.elasticsearch.client
 
-import groovy.transform.TypeChecked
-
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest
 import org.elasticsearch.client.Client
 import org.elasticsearch.client.transport.TransportClient
-import org.elasticsearch.common.settings.ImmutableSettings
 import org.elasticsearch.common.settings.Settings
 import org.elasticsearch.common.transport.InetSocketTransportAddress
 import org.elasticsearch.common.xcontent.json.JsonXContent
-import org.junit.Test
-import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import com.elasticsearch.client.dao.TapacrossDao
 import com.elasticsearch.client.entity.TableEntity
 
+import groovy.transform.TypeChecked
+
 @TypeChecked
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:spring/application-context.xml")
-class TestApp {
+class DocumentInserter2 {
+	private def start
+	Client client
+	final String INDEX_NAME1 = "twitter1712"
+	final String TYPE_NAME1 = "mytype"
+	final String FIELD1_NAME = "articleId"
+	final String FIELD2_NAME = "title"
+	final String FIELD3_NAME = "body"
+	final String FIELD4_NAME = "createDate"
+	
+	private ApplicationContext context;
+	
 	@Autowired
 	private TapacrossDao dao
 
-	static Client client
-	static final String INDEX_NAME1 = "myindex"
-	static final String TYPE_NAME1 = "mytype"
-	static final String TYPE_NAME2 = "mytype2"
-	static final String FIELD1_NAME = "articleId"
-	static final String FIELD2_NAME = "title"
-	static final String FIELD3_NAME = "body"
-	static final String FIELD4_NAME = "createDate"
-	static final String FIELD5_NAME = "siteType"
-
-	@Test
 	void run() {
+		this.context = new GenericXmlApplicationContext(
+			"classpath:spring/application-context.xml");
+		dao = context.getBean(TapacrossDao.class)
+		
 		createClient()
-		deleteIndex(INDEX_NAME1)
-		createIndex(INDEX_NAME1)
-		createMapping(INDEX_NAME1, TYPE_NAME1)
-//		createMapping(INDEX_NAME1, TYPE_NAME2)
+		
+//		deleteIndex(INDEX_NAME1)
+//		createIndex(INDEX_NAME1)
+//		createMapping(INDEX_NAME1, TYPE_NAME1)
 		
 		def start = System.currentTimeMillis()
 		println "start add documents"
-		addDocument("tb_article_search_twitter_1712", INDEX_NAME1, TYPE_NAME1)
-//		addDocument("tb_article_search_twitter_1711", INDEX_NAME1, TYPE_NAME2)
-//		addDocument("tb_article_search_media_1712", INDEX_NAME2, TYPE_NAME1)
-//		addDocument("tb_article_search_media_1711", INDEX_NAME2, TYPE_NAME2)
+		
+		createThreads()
+		
+		sleep(1000 * 60 * 60 * 24)
+//		addDocument("tb_article_search_twitter_1711", INDEX_NAME1, TYPE_NAME1)
 		println "end add document. elasped:${(System.currentTimeMillis() - start) / 1000}s."
 	}
-
-	static void createClient() {
-		def s = ImmutableSettings.settingsBuilder()
+	
+	int sequence = 1
+	void createThreads() {
+		12.times {
+			Thread.start {
+				while (true) {
+					addDocument("tb_article_search_twitter_1712", INDEX_NAME1, TYPE_NAME1)
+				}
+			}
+		}
+	}
+	int makeSequence() {
+		def fetch = 10000
+		def end = sequence + fetch
+		sequence = end
+		
+		return end
+		
+	}
+	void createClient() {
+		def s = Settings.settingsBuilder()
 				.put("cluster.name", "elasticsearch").build();
-		def tmp = new TransportClient(s);
-		tmp.addTransportAddress(new InetSocketTransportAddress("210.122.10.31",
-				9300));
+		def tmp = TransportClient.builder().settings(s).build();
+		tmp.addTransportAddress(
+			new InetSocketTransportAddress(
+				new InetSocketAddress("121.254.177.193", 9300)));
 		client = tmp;
 	}
 	void createIndex(String indexName) {
@@ -79,12 +98,13 @@ class TestApp {
 		}
 	}
 	void createMapping(String indexName, String typeName) {
-		// myindex:{
-		//	mappings:{
-		//		mytype:{
-		//			properties:{
-		//				fieldName1:{
-		//					type:long, analyzer:keyword
+		// properties:{
+		// 	field1:{
+		//		type:string
+		// }, field2:{
+		// 		type:string
+		// }
+		//}
 		def builder = JsonXContent.contentBuilder().
 				startObject().field(typeName)
 					.startObject().field("properties")
@@ -101,9 +121,6 @@ class TestApp {
 								.field(FIELD4_NAME)
 									.startObject().field("type", "date").field("format", "yyyyyMMddHHmmss")
 									.endObject()
-								.field(FIELD5_NAME)
-									.startObject().field("type", "string").field("analyzer", "keyword")
-									.endObject()
 						.endObject()
 					.endObject()
 				.endObject()
@@ -115,29 +132,45 @@ class TestApp {
 	}
 	
 	void addDocument(String tableName, String indexName, String typeName) {
-		def fetch = 100
+		def fetch = 10000
 		def start = 0
-		def end = start + fetch
-		def dest = 1000
-		while (start < dest) {
+		def end = 0
+		synchronized(this) {
+			start = sequence
+			end = makeSequence()
+		}
+		def dest = 180000000
+		if (start >= dest) {
+			println "dest:$start"
+			System.exit(1)
+		}
+		
+		if (start < dest) {
 			def startTime = System.currentTimeMillis()
 			def bulker = client.prepareBulk()
 			def result = dao.selectArticles(start, end, tableName)
 			result.each { TableEntity it ->
+				def currentIndexName = "twitter" + it.createDate.substring(2, 8)
 				def ir = client.prepareIndex(indexName, typeName, it.seq.toString())
-				.setSource(FIELD1_NAME, it.articleId, 
-					FIELD2_NAME, it.title, FIELD3_NAME, it.body,
-					FIELD4_NAME, it.createDate, FIELD5_NAME, it.siteType) 
+				.setSource(FIELD1_NAME, it.articleId,
+					FIELD2_NAME, "", FIELD3_NAME, it.body,
+					FIELD4_NAME, it.createDate)
 				bulker.add(ir)
 			}
 			bulker.execute().actionGet()
 			
 			def elasped = (System.currentTimeMillis() - startTime) / 1000
 			println "add $tableName, start:$start, end:$end, elapsed:$elasped"
-			start = end + 1
-			end = end + fetch
+//			start = end + 1
+//			end = end + fetch
 		}
 	}
 	static void updateDocument() {
+		//		client.prepareUpdate(INDEX_NAME, TYPE_NAME, "1").setScript(ScriptService.ScriptType.INLINE)
+	}
+	
+	static void main(args) {
+		def insert = new DocumentInserter2()
+		insert.run()
 	}
 }
