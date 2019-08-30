fed09c36f2c167b4a0d7aa1cb2807e702628a03f
cuckoo03@gmail.com <wolfen11>
2019-08-30 20:53:45 +0900

    #

---
+++ es1-study/src/main/java/com/elasticsearch/es1_study/App.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.es1_study;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
