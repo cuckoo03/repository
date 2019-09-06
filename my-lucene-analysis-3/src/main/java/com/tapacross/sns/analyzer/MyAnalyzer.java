89827dfcbc6e719afb0854ea2896dcbb8c5df886
cuckoo03@gmail.com <wolfen11>
2019-09-06 21:59:33 +0900

    #

---
+++ my-lucene-analysis-3/src/main/java/com/tapacross/sns/analyzer/MyAnalyzer.java
@@ -0,0 +1,28 @@
+package com.tapacross.sns.analyzer;
+
+import java.io.Reader;
+import java.util.logging.Logger;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Analyzer.ReuseStrategy;
+import org.apache.lucene.analysis.Analyzer.TokenStreamComponents;
+
+/**
+ * 엘라스틱서치에서 색인시 사용하는 분석기
+ * 텍스트가 입력될 때마다 해당 텍스트를 사전서버에서 검색하여 토큰목록을 MyTokenizer에
+ * 넘겨주어야 정상동작 한다.
+ * 
+ * @author admin
+ *
+ */
+public class MyAnalyzer extends Analyzer {
+	public MyAnalyzer() {
+		super(new MyGlobalReuseStrategy());
+	}
+	
+	@Override
+	protected TokenStreamComponents createComponents(String fieldName, Reader reader) {
+		return new TokenStreamComponents(new MyTokenizer(reader));
+	}
+}
