53dc1c8a715e9ecdac984d50a9c47c7f7bc4b881
cuckoo03 <wolfen11>
2019-12-07 15:52:43 +0900

    #

--- groovy-study/src/main/groovy/com/groovy/release/r2_2/ImplicitClosureExam.groovy
+++ groovy-study/src/main/groovy/com/groovy/release/r2_2/ImplicitClosureExam.groovy
@@ -6,7 +6,7 @@ import groovy.transform.TypeChecked;
 class ImplicitClosureExam {
 	static main(args) {
 		def input = [1, 2, 3, 4, 5]
-		def odd = filter(input) { int it -> it % 2 == 1 }
+		def odd = filter(input) { it -> (it as int) % 2 == 1 }
 		assert odd == [1, 3, 5]
 
 		def odd2 = filter(input, { (it as int) % 2 == 1} as Predicate)
